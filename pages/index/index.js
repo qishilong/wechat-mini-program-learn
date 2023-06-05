63fa242f62519093ae1ada15c34891a64b57a70d
qishilong <663253062@qq.com>
2023-06-05 21:53:16 +0800

    小程序初次练习
    小程序初次练习

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,45 +4,54 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
+    time: new Date().toString(),
+    name: 'xiaozhi',
+    a: 10,
+    b: 11,
+    c: 10,
+    isShow:false,
+    listShow: [1,2,3,4],
+    listString:'xiaoming',
+    listCustom: ['xiaoming', 'xiaozhi', 'xiaohong', 'xiaohua'],
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4],
+    importTempleteData: {
+      name: Math.random() > Math.random() ? 'itemA' : 'itemB',
+      age : Math.random() < Math.random() ? 17 : 18
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+  switch: function(e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    // this.data.objectArray = this.data.objectArray.sort(()=>Math.random() - 0.5)
+    this.setData({
+      objectArray: this.data.objectArray
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+  addToFront: function(e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function(e){
+    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
     })
   }
 })
